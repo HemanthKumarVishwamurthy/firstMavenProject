c84871027ce5b84c9c0c42c00ad2d1e0bd001ea5
kumarh14 <Hemanth_kumar_v.Kumar_v@emc.com>
2019-09-27 07:55:08 +0530

    first commit

---
+++ src/main/java/com/learning/firstMavenProject/StudentMain.java
@@ -0,0 +1,16 @@
+package com.learning.firstMavenProject;
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class StudentMain {
+
+	public static void main(String[] args) {
+		ApplicationContext appCon = new ClassPathXmlApplicationContext("StudentConfig.xml");
+		Student student = (Student) appCon.getBean("studentBean");
+		student.print();
+		
+	}
+
+}
